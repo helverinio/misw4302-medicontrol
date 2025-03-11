25691a813317b0baa4ea10ae922a2b6ae57f857e
helverinio <h.wiesner@uniandes.edu.co>
2025-03-10 21:50:01 -0500

    implement form validation for login inputs; add error highlighting for empty fields

--- web_medicontrol/src/app/pages/login/login.component.ts
+++ web_medicontrol/src/app/pages/login/login.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  isEmailError = false;
+  isPasswordError = false;
+
+  onSubmit() {
+    const emailInput = document.getElementById('email') as HTMLInputElement;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+    if (!emailInput.value) {
+      this.isEmailError = true;
+    } else {
+      this.isEmailError = false;
+    }
+
+    if (!passwordInput.value) {
+      this.isPasswordError = true;
+    } else {
+      this.isPasswordError = false;
+    }
+  }
 }
