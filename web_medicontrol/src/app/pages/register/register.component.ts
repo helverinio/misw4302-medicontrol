82197baaa073ffb6cdd910f0c524198f18de05bf
helverinio <h.wiesner@uniandes.edu.co>
2025-03-10 22:08:13 -0500

    add registration page with form fields and validation; update login page with navigation link

--- web_medicontrol/src/app/pages/register/register.component.ts
+++ web_medicontrol/src/app/pages/register/register.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  isNameError = false;
+  isLastNameError = false;
+  isEmailError = false;
+  isPasswordError = false;
+
+  onSubmit() {
+  }
 }
